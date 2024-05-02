1257d0d55bee569d7f0b9ae97354048a35a41bac
kurono <kurono@riseup.net>
2024-05-02 10:48:39 -0600

    modify slidebar menu

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,24 +1,43 @@
 const sidebars = {
   tutorialSidebar: [
     {
+    {
+      type: "category",
+      label: "Welcome",
+      items: ["welcome-to-sakundi.md"],
+    },
+
+      type: "category",
+      label: "Sakundi for Validator nodes",
+      items: ["accessing-sakundi-dashboard.md",
+              "registration-and-account-setup.md",
+              "managing-validator-nodes.md",
+              "understanding-metrics-and-analytics.md",
+              "customization-and-configuration.md",
+              "real-time-alerts-and-notifications.md",
+              "support-and-resources.md"],
+    },
+    {
+      type: "category",
+      label: "Tikuna - our Open Source base",
+      items: ["user-section/how-to-use-tikuna",
+              "user-section/understanding-dashboards",
+              "monitoring/network-eth2",
+              "monitoring/beacon-node",
+              "monitoring/eclipse-attacks"],
+    },
+    {
       type: "category",
       label: "Research",
-      items: ["research/intro", "research/stateoftheart","research/references","research/glossary"],
+      items: ["research/intro",
+              "research/stateoftheart",
+              "research/references",
+              "research/glossary"],
     },
     {
       type: "category",
       label: "About",
       items: ["about/team", "about/sakundi"],
-    },
-    {
-      type: "category",
-      label: "Monitoring",
-      items: ["monitoring/network-eth2", "monitoring/beacon-node", "monitoring/eclipse-attacks"],
-    },
-    {
-      type: "category",
-      label: "User Section",
-      items: ["user-section/how-to-use-tikuna", "user-section/understanding-dashboards"],
     },
   ],
 };
